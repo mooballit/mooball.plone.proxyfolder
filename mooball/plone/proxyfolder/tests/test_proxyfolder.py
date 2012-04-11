c0d098853679aedefe26247106024b8ac180ae30
Roman Joost <roman@mooball.com>
2012-04-11 14:13:36 +1000

    Test the object if it implements it's interface.

--- mooball/plone/proxyfolder/tests/test_proxyfolder.py
+++ mooball/plone/proxyfolder/tests/test_proxyfolder.py
@@ -1,7 +1,10 @@
 # Copyright (c) 2012 Mooball IT
 # See also LICENSE.txt
 from mooball.plone.proxyfolder.testing import PROXYFOLDER_FUNCTIONAL_TESTING
+from mooball.plone.proxyfolder.types.proxyfolder import IProxyFolder
+from plone.dexterity.factory import DexterityFactory
 import unittest
+import zope.interface
 
 
 class TestProxyFolder(unittest.TestCase):
@@ -9,4 +12,8 @@ class TestProxyFolder(unittest.TestCase):
     layer = PROXYFOLDER_FUNCTIONAL_TESTING
 
     def test_pass(self):
-        self.assertTrue(1)
+        factory = DexterityFactory(
+            portal_type='mooball.plone.proxyfolder.types.proxyfolder')
+        self.assertTrue(
+            zope.interface.verify.verifyObject(
+                IProxyFolder, factory()))
